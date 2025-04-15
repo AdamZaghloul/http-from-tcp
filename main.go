f85d43da91b7221aa3c5679e7a09da9ac6c6b3f6
AdamZaghloul <adam.zaghloul95@gmail.com>
2025-04-15 09:53:39 -0400

    8 bytes at a time print line by line.

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("./messages.txt")
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+
+	offset := 0
+	b := make([]byte, 8)
+	line := ""
+
+	for err == nil {
+		_, err = file.ReadAt(b, int64(offset))
+		parts := strings.Split(string(b), "\n")
+		line += parts[0]
+		if len(parts) > 1 {
+			fmt.Printf("read: %s\n", line)
+			line = parts[1]
+		}
+		offset += 8
+	}
 }
