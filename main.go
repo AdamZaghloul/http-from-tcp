09de2921013081c4642b96dcf44634ebc5f7471b
AdamZaghloul <adam.zaghloul95@gmail.com>
2025-04-27 19:18:04 -0400

    Added tcp call functionality.

--- main.go
+++ main.go
@@ -1,30 +1,70 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"os"
+	"io"
+	"net"
 	"strings"
 )
 
 func main() {
-	file, err := os.Open("./messages.txt")
+	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
 		fmt.Print(err.Error())
 		return
 	}
 
-	offset := 0
-	b := make([]byte, 8)
-	line := ""
+	defer listener.Close()
 
-	for err == nil {
-		_, err = file.ReadAt(b, int64(offset))
-		parts := strings.Split(string(b), "\n")
-		line += parts[0]
-		if len(parts) > 1 {
-			fmt.Printf("read: %s\n", line)
-			line = parts[1]
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
 		}
-		offset += 8
+
+		fmt.Println("Connection accepted.")
+
+		ch := getLinesChannel(conn)
+
+		for line := range ch {
+			fmt.Printf("%s\n", line)
+		}
+
+		fmt.Println("Connection closed.")
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+
+	ch := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+		currentLineContents := ""
+		for {
+			b := make([]byte, 8, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if currentLineContents != "" {
+					ch <- currentLineContents
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+			str := string(b[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				ch <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
+		}
+	}()
+
+	return ch
 }
